52dc1a7d1112a3fa22506323a6554ffd07a8af07
cbbrstn <christophbieberstein@gmail.com>
2017-06-12 22:45:18 +0200

    New webcams can be added via form & json responses implemented

--- app/controllers/webcams_controller.rb
+++ app/controllers/webcams_controller.rb
@@ -1,17 +1,59 @@
 class WebcamsController < ApplicationController
+  before_action :set_webcam, only: [:show, :edit, :update]
+
+  # respond_to :html, :json
+
   def index
     @webcams = Webcam.all
+
+    respond_to do |format|
+            format.html
+            format.json { render :json => @webcams }
+        end
+
   end
 
   def show
+
+    respond_to do |format|
+            format.html
+            format.json { render :json => @webcam }
+        end
+
   end
 
   def create
+
+    @webcam = Webcam.new(webcam_params)
+
+    if @webcam.save
+      redirect_to root_path
+    end
+
+  end
+
+  def new
+    @webcam = Webcam.new
+
   end
 
   def edit
+
   end
 
   def update
   end
+
+  private
+
+  def webcam_params
+    params.require(:webcam).permit(:name, :url, :location)
+
+  end
+
+  def set_webcam
+      @webcam = Webcam.find(params[:id])
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   root 'webcams#index'
 
   resources :webcams
+  #, defaults: {format: :json}
+
+  get 'webcams/new' => 'webcams#new'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
